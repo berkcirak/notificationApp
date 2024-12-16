42ef18fd389603804539dc03db5f5decb032f7e0
berk cirak <atalayberkcrak@gmail.com>
2024-12-16 10:52:17 +0300

    service layer added

--- notificationApp/src/main/java/com/example/notificationApp/entity/User.java
+++ notificationApp/src/main/java/com/example/notificationApp/entity/User.java
@@ -1,25 +1,59 @@
 package com.example.notificationApp.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "tbl_users")
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Data
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String username;
+    private String email;
     private String password;
 
+    public User(int id, String username, String email, String password) {
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+    }
+
+    public User() {
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- notificationApp/src/main/java/com/example/notificationApp/service/UserService.java
+++ notificationApp/src/main/java/com/example/notificationApp/service/UserService.java
@@ -1,6 +1,47 @@
 package com.example.notificationApp.service;
 
+import com.example.notificationApp.entity.User;
+import com.example.notificationApp.entity.UserDTO;
 import com.example.notificationApp.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
-public class UserService  {
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    private UserRepository userRepository;
+
+    public UserService(UserRepository userRepository){
+        this.userRepository=userRepository;
+    }
+
+    public User createUser(User user){
+        return userRepository.save(user);
+    }
+    public List<User> getUsers(){
+        return userRepository.findAll();
+    }
+    public Optional<User> getUser(int userId){
+        return userRepository.findById(userId);
+    }
+    public User updateUser(UserDTO currentUser, int userId){
+        User userOptional=userRepository.findById(userId).orElseThrow(() -> new RuntimeException("user not found"));
+        if (currentUser.getUsername() != null){
+            userOptional.setUsername(currentUser.getUsername());
+        }
+        if (currentUser.email != null){
+            userOptional.setEmail(currentUser.getEmail());
+        }
+        if (currentUser.getPassword() != null){
+            userOptional.setPassword(currentUser.getPassword());
+        }
+        return userRepository.save(userOptional);
+    }
+    public void deleteUser(int userId){
+        userRepository.deleteById(userId);
+    }
+
+
 }
