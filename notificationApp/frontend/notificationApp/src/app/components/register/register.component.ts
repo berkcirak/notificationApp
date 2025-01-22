65f3f10aae90d35db9478a13087d547f2899654e
berk cirak <atalayberkcrak@gmail.com>
2025-01-22 17:13:38 +0300

    added new design patterns

--- notificationApp/frontend/notificationApp/src/app/components/register/register.component.ts
+++ notificationApp/frontend/notificationApp/src/app/components/register/register.component.ts
@@ -24,27 +24,25 @@ export class RegisterComponent {
     this.countries = Countries.countries;
   }
 
-  register(registerForm: any) {
-    if (registerForm.invalid) {
+
+  register(registerForm: any){
+    if(registerForm.invalid){
       alert('Lütfen tüm alanları eksiksiz doldurun.');
       return;
     }
-  
-    this.authService.register(this.user).subscribe(
-      (res: any) => {
-        if (res) {
-          localStorage.setItem('token', res);
-          this.router.navigate(['/homepage']).then(() => {
-            window.location.reload();
-          });
-        }
-      },
-      (error) => {
-        console.error('Register failed:', error);
+
+    this.authService.register(this.user).subscribe((res: any) => {
+      if(res){
+        localStorage.setItem('token', res);
+        this.router.navigate(['/homepage']).then(() => {
+          window.location.reload();
+        });
       }
-    );
+    }, error => {
+      console.error('register failed: ', error);
+    })
   }
-  
+
 
   goToLogin(){
     this.router.navigate(['/login']);
