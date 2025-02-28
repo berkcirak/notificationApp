7aea326d7a48335e3eb36765340d60e139df878d
Berk Çırak <102749563+berkcirak@users.noreply.github.com>
2025-03-01 00:00:36 +0300

    Update web-scraping.py

--- notificationApp/beautifulSoup/web-scraping.py
+++ notificationApp/beautifulSoup/web-scraping.py
@@ -13,7 +13,7 @@ def scrape_product():
         return jsonify({"error": "No URL provided"}), 400
 
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36"
+        "User-Agent": "agenInfo"
     }
 
     try:
